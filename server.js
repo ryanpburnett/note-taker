ce538757b70fa70990bde8f1254e1710056e959c
Ryan <ryanpburnett@yahoo.com>
2021-03-18 13:58:12 -0500

    static folder set, db.json displaying

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'))
