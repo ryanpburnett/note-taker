99625da360b100f9365a02c23a9a40a66791c5c1
Ryan <ryanpburnett@yahoo.com>
2021-03-18 12:01:42 -0500

    got rid of unused code

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const path = require('path')
 const express = require('express');
 const app = express();
@@ -13,18 +14,6 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/notes.html'))
 })
-
-// app.post('/result', (req, res) => {
-//     if (req.body.color.trim().toLowerCase() === "blue") {
-//         res.send("correctamundo")
-//     }else{
-//         res.send("not lookin' so good")
-//     }
-// })
-
-// app.get('/result', (req, res) => {
-//     res.send("What the hell are you doing here?  You don't belong here.")
-// })
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT} - localhost:${PORT}`)
