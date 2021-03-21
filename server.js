947f5eb07ac293edbec290fff9cb0a4c1141f963
Ryan <ryanpburnett@yahoo.com>
2021-03-21 09:58:21 -0500

    getting db.json

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
-})
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/notes.html'))
-})
+    res.sendFile(path.join(__dirname + '/public/notes.html'));
+});
+
+// app.get('/api/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/db/db.json'))
+// });
+    
+app.post('/api/notes', (req, res) => {
+
+});
 
 app.listen(PORT, err => {
     if (err) {
         console.log(err);
     }
-    console.log(`listening on http://localhost:${PORT}`)
+    console.log(`listening on http://localhost:${PORT}`);
 });
